a64e60f39de523a374e3370c733423548d129e84
samuelnwokike27@gmail.com <samuelnwokike27@gmail.com>
2025-09-11 23:33:12 +0100

    first commit

---
+++ js/oAuth.js
@@ -0,0 +1,7 @@
+// if(!localStorage.getItem("key")) {
+//     const token = localStorage.getItem("token")
+//     if(!token || token.trim() === "") {
+//         window.location.href = "error.html"
+//     }
+    
+// }
